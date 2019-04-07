1b491d174094f7422c9e82d2877ecc9464639c64
Rafael Cerri <rafaelbcerri@gmail.com>
2019-04-06 22:09:20 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'reactdom';
-// Inject a theme into your application
-import { MuiThemeProvider } from '@materialui/core/styles';
-// Our theme
-import Theme from './components/Theme';
-import App from './App';
-
-ReactDOM.render(
-  <MuiThemeProvider theme={Theme}>
-    <App />
-  </MuiThemeProvider>,
-
-  document.getElementById('root'),
-);
+// import React from 'react';
+// import ReactDOM from 'reactdom';
+// // Inject a theme into your application
+// import { MuiThemeProvider } from '@materialui/core/styles';
+// // Our theme
+// import Theme from './components/Theme';
+// import App from './App';
+//
+// ReactDOM.render(
+//   <MuiThemeProvider theme={Theme}>
+//     <App />
+//   </MuiThemeProvider>,
+//
+//   document.getElementById('root'),
+// );
